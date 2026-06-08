148af74cd84156d7a410158adba0ece0e9564d43
KABILAN <Kabilankabi386@gmail.com>
2026-06-08 12:35:52 +0530

    feat: add sliding window solution for Maximum Average Subarray problem

--- DSA-main/DSA_pattern/slidingWindow/MaximumAverageSubarry.py
+++ DSA-main/DSA_pattern/slidingWindow/MaximumAverageSubarry.py
@@ -1,3 +1,6 @@
+# Question : Find the maximum average of the subarray of size k.
+# If no such subarray exists, return 0.
+
 from typing import List
 
 nums = [1,12,-5,-6,50,3]
@@ -18,3 +21,12 @@ def findMaxAverage(nums, k):
 
 
 print(findMaxAverage(nums,k))
+
+
+# Loops
+# for i in range(k,len(nums)) - this loop is used to iterate through the array
+
+
+# Time and space complexity
+# Time complexity : O(n)
+# Space complexity : O(1)
