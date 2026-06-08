cfc83229fb726f1a5fe86d3cb63d25ee98fae2a6
KABILAN <Kabilankabi386@gmail.com>
2026-06-08 14:27:56 +0530

    feat: initialize FastAPI application with root, about, and user creation endpoints

--- DSA-main/python_interview/main.py
+++ DSA-main/python_interview/main.py
@@ -9,3 +9,13 @@ def read_root():
 @app.get("/about")
 def read_about():
     return {"message": "About Page"}
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+
+@app.post("/users")
+def create_user(user: User):
+    return user
