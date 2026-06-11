fe9ad4481edb6d251fe7b87f700830983ab9a5f2
Kabilan <kabilankabi386@gmail.com>
2026-06-11 21:18:54 +0530

    feat: add demonstration script for variable declaration and array mutation in JavaScript

---
+++ DSA-main/js_interview/variableDecleration.js
@@ -0,0 +1,22 @@
+const arr = [1,2,3,4,5]
+
+arr.push(6)
+
+var arr1 = [1,2,3,4,5]
+
+arr.push(6)
+
+let arr2 = [1,2,3,4,5]
+
+arr2.push(6)
+
+console.log(arr)
+console.log(arr2)
+console.log(arr1)
+
+
+// const arr = [1,2,3,4,5]
+
+// let arr2 = [1,2,3,4,5]
+
+
