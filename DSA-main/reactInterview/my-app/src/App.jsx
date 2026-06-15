df2b493c1f2bb9a16bbf1cc87307fd2fdf003dd3
Kabilan <kabilankabi386@gmail.com>
2026-06-15 21:16:23 +0530

    feat: implement theme context and toggle functionality in App component

--- DSA-main/reactInterview/my-app/src/App.jsx
+++ DSA-main/reactInterview/my-app/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import  {mockApi}  from './api'
-
+import { useTheme } from './ThemeContex';
 
 function App() {
-  
+  const { theme, toggleTheme } = useTheme();
   const [data, setData] = useState([]);
 
   const handleClick = async () => {
@@ -22,7 +22,13 @@ function App() {
   return (
     <>
      <div>
+      <h1>Current Theme: {theme}</h1>
+      <button onClick={toggleTheme}>
+        Toggle Theme
+      </button>
+     </div>
 
+    <div className={`app-container ${theme}`}>
       <button onClick={handleClick}>
         Load Data
       </button>
