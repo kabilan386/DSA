cf4add906c8cb8c080aaf08efc8b2b63726c162e
Kabilan <kabilankabi386@gmail.com>
2026-06-19 13:03:30 +0530

    feat: add python scripts for math operations, higher-order functions, and list concatenation

---
+++ DSA-main/python_interview/HOF.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    return a + b
+
+def apply_func(func,a,b):
+    return func(a,b)
+
+print(apply_func(add,1,2))
---
+++ DSA-main/python_interview/math.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.sqrt(16))
+
+n = 3.7
+
+F_num = math.floor(n)
+
+print(5//2)
+print(5/2)
