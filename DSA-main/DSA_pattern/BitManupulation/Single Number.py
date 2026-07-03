479b22ccdfa099bfef86dd105e999095398d0b46
Kabilan <kabilankabi386@gmail.com>
2026-07-03 13:24:39 +0530

    feat: implement singleNumber function to find the unique number in an array

---
+++ DSA-main/DSA_pattern/BitManupulation/Single Number.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+
+        for i in nums:
+            a ^= i
+
+        return a
+    
+
+print(Solution().singleNumber([2,2,1]))
+    
